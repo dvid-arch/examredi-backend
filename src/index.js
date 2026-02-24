fba1c52ba80e2002e16c2f904747f7455fb074db
dvid-arch <derrickemma44@gmail.com>
2026-02-24 07:44:20 +0100

    increase json payload limit

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Request logging middleware
 app.use((req, res, next) => {
